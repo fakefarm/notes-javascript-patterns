5aa510f9b4865687d0ffaa1650b2a4d856984db1
Dave Woodall <dave.woodall@livingsocial.com>
2015-12-14 10:58:32 -0700

    Vars can provide DOM context
    The notion of using vars to acquire DOM content is interesting. At first
    I considered using vanilla JS but realized that I will always opt for
    JQuery when I’m interacting with the DOM. So I’m not going to be so
    dogmatic here. I struggled a little with scope and how to create a new node in the DOM
    I needed to use `attr()` and then was able to chain together my DOM
    commands.

--- jasmine-standalone-2.3.4/spec/PlayerSpec.js
+++ jasmine-standalone-2.3.4/spec/PlayerSpec.js
@@ -1,32 +1,66 @@
+
 var windowVar = this;
+
 describe("Chapter 1", function() {
-  describe("pg. 10 - minimizing globals", function() {
-    it("'this' is a global that is the same as window", function() {
-      expect(windowVar).toBe(window);
+  describe('On global values', function() {
+    describe("pg. 10 - minimizing globals", function() {
+      it("'this' is a global that is the same as window", function() {
+        expect(windowVar).toBe(window);
+      });
+      it("variables without a var will be global", function() {
+        myglobal = 'hello'; // noted as an antipattern
+        expect(myglobal).toBe(window.myglobal);
+      });
     });
-    it("variables without a var will be global", function() {
-      myglobal = 'hello'; // noted as an antipattern
-      expect(myglobal).toBe(window.myglobal);
+
+    describe("pg. 11 - the problem with globals", function() {
+      it("they are easy to create", function() {
+        function sum(x, y) {
+          result = x + y;
+          return result;
+        }
+        var two = sum(1,1);
+        expect(window.result).toBe(two);
+      });
+
+      it("but you can prevent global variables by using var", function() {
+        function sum(x, y) {
+          var localVar = x + y;
+          return localVar;
+        }
+        var three = sum(2,1);
+        expect(window.localVar).not.toBe(three);
+      });
     });
   });
 
-  describe("pg. 11 - the problem with globals", function() {
-    it("they are easy to create", function() {
-      function sum(x, y) {
-        result = x + y;
-        return result;
-      }
-      var two = sum(1,1);
-      expect(window.result).toBe(two);
+  describe('assign DOM references to local variables', function() {
+
+    var $div  = '',
+        el    = '',
+        style = '';
+
+    afterEach(function() {
+       $div.remove();
     });
 
-    it("but you can prevent global variables by using var", function() {
-      function sum(x, y) {
-        var localVar = x + y;
-        return localVar;
+    it('helps setup local environment to work with.', function() {
+
+      $div = $("<div id='result'>hello world</div>").
+                attr({id: 'result'}).
+                css({color: 'red'}).
+                appendTo('body');
+
+      function updateElement() {
+        el = document.getElementById("result");
+        style = el.style.color;
       }
-      var three = sum(2,1);
-      expect(window.localVar).not.toBe(three);
+
+      updateElement();
+      expect(style).toBe('red');
     });
   });
 });
+
+
+
